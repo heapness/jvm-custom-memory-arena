b6646863d58b6a11e913e664c79913c8ca6ffcb3
masonliiu <liumasn@gmail.com>
2025-12-19 12:12:45 -0600

    basic representation of manual memory allocation

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        MemoryArena arena = new MemoryArena(128);
+
+        int a = arena.alloc(4);
+        int b = arena.alloc(4);
+        int c = arena.alloc(4);
+
+        System.out.println("address of a: " + a);
+        System.out.println("address of b: " + b);
+        System.out.println("address of c: " + c);
+    }
 }
--- MemoryArena.java
+++ MemoryArena.java
@@ -1,3 +1,19 @@
 public class MemoryArena {
-    
+    private final byte[] memory;
+    private int offset = 0;
+
+    public MemoryArena(int size) {
+        memory = new byte[size];
+    }
+
+    public int alloc(int size) {
+        if (size + offset > memory.length) {
+            throw new RuntimeException("Out of memory!");
+        }
+
+        int start = offset;
+        offset += size;
+        return start;
+    }
+
 }
